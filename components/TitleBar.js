1823f3c916e5e8a46c863a747439b7e7b3f74a19
Cameron McGuinness <cam@cammcguinness.com>
2017-08-06 12:42:06 -0400

    Fixes #4. Weather is now displayed on title bar

--- components/TitleBar.js
+++ components/TitleBar.js
@@ -14,17 +14,21 @@ import {connect} from 'react-redux';
 import * as Actions from '../actions';
 import {formatValue} from '../utilities/util';
 
-function mapStateToProps(state)  {return {resources: state.stepReducers.resources};}
+function mapStateToProps(state)  {return {resources: state.stepReducers.resources,weather: state.stepReducers.weather};}
 function mapDispatchToProps(dispatch) { return bindActionCreators(Actions, dispatch);}
 
 class TitleBar extends React.Component {
 
 
   render() {
+    //console.log(this.props.weather?this.props.weather.weather[0].description:"Weather is null");
     return (
       <View style={styles.titleBar}>
         <MonoText style={styles.title}>Dragon Go</MonoText>
-
+        <View style={styles.goldContainer}>
+          <MonoText style={styles.weather}>{this.props.weather?this.props.weather.main.temp:""}C</MonoText>
+          <MonoText style={styles.weather}>{this.props.weather?this.props.weather.weather[0].description.replace(/\b\w/g, l => l.toUpperCase()):""}</MonoText>
+        </View>
         <View style={styles.goldContainer}>
           <MonoText style={styles.gold}>Gold: {formatValue(this.props.resources.gold)}</MonoText>
           <MonoText style={styles.gold}>Food: {formatValue(this.props.resources.food)}</MonoText>
@@ -56,6 +60,10 @@ const styles = StyleSheet.create({
   gold: {
     fontSize: 14,
     color: 'gold',
+  },
+  weather: {
+    fontSize: 14,
+    color: 'white',
   },
   foodContainer: {
     flexDirection: 'column',
